792681bcd7d4fc22afa62bd0305df41584730d81
Dylan McVay <dfmcvay@gmail.com>
2019-06-23 14:13:44 -0400

    Added Binary Search

---
+++ DSandAlgorithms/Algorithms/Searching/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSandAlgorithms.Algorithms.Searching
+{
+    public class BinarySearch
+    {
+        public static bool Search<T>(T[] array, T target) where T : IComparable
+        {
+            return SearchCore(array, target, 0, array.Length - 1);
+        }
+
+        private static bool SearchCore<T>(T[] array, T target, int left, int right) where T : IComparable
+        {
+
+            if(left > right)
+            {
+                return false;
+            }
+
+            int midPoint = left + ((right - left) / 2);
+            int cmp = array[midPoint].CompareTo(target);
+
+            if(cmp == 0)
+            {
+                return true;
+            }
+
+            if(cmp > 0)
+            {
+                return SearchCore(array, target, left, midPoint - 1);
+            }
+            else
+            {
+                return SearchCore(array, target, midPoint + 1, right);
+            }
+        }
+    }
+}
---
+++ DSandAlgorithmsTests/Algorithms/Searching/BinarySearchTest.cs
@@ -0,0 +1,21 @@
+﻿using DSandAlgorithms.Algorithms.Searching;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace DSandAlgorithmsTests.Algorithms.Searching
+{
+    [TestClass]
+    public class BinarySearchTest
+    {
+        [TestMethod]
+        public void SortTest()
+        {
+            int[] input = new int[] { 1, 5, 10, 25, 2000, 3550 };
+
+            Assert.AreEqual(true, BinarySearch.Search(input, 2000));
+            Assert.AreEqual(false, BinarySearch.Search(input, 35));
+            Assert.AreEqual(true, BinarySearch.Search(input, 1));
+        }
+    }
+}
